70bb7bbdb2fc0141f583ae64ac42b884fa61d172
Tianxiang Chen <curlfw@gmail.com>
2013-05-30 23:29:38 -0700

    cleanup Q004

--- LeetSharp/Q004_MedianofTwoSortedArrays.cs
+++ LeetSharp/Q004_MedianofTwoSortedArrays.cs
@@ -18,63 +18,6 @@ namespace LeetSharp
             return -1f;
         }
 
-        public int GetNthFrom2Arrays(int[] a, int[] b, int index)
-        {
-            int left = 0;
-            int right = a.Length - 1;
-            while (true)
-            {
-                int currentIndexA = (left + right) / 2;
-                int j = index - currentIndexA - 1;
-                
-                if (j >= b.Length)
-                {
-                    left = currentIndexA + 1; // j too big, search right part (cA increase then j decrease)
-                }
-                else if (j < -1)
-                {
-                    right = currentIndexA - 1;
-                }
-                else if (j == -1)
-                {
-                    if (a[currentIndexA] <= b[0])
-                    {
-                        return a[currentIndexA];
-                    }
-                    else
-                    {
-                        right = currentIndexA - 1;
-                    }
-                }
-                else // j valid
-                {
-                    if (a[currentIndexA] >= b[j])
-                    {
-                        // if a[cA] >= b[j]  && a[cA] <= b[j+1], then a[cA] is the answer
-                        if (j == b.Length - 1 || a[currentIndexA] <= b[j + 1])
-                        {
-                            return a[currentIndexA];
-                        }
-                        else // a[cA] is greater than answer
-                        {
-                            // find in left part in a[]
-                            right = currentIndexA - 1;
-                        }
-                    }
-                    else // a[cA] is smaller than answer
-                    {
-                        // find in right part in a[]
-                        left = currentIndexA + 1;
-                    }
-                }
-
-                if (left > right) // answer not in a
-                {
-                    return GetNthFrom2Arrays(b, a, index);
-                }
-            }
-        }
-
         public string SolveQuestion(string input)
         {
             return FindMedianSortedArrays(input.GetToken(0).ToIntArray(), input.GetToken(1).ToIntArray()).ToString("F5");
